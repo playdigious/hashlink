db579ae960697ae7f4332325f8d000507f183eaf
Romain TISSERAND <romain@iMac-de-Romain.local>
2017-11-15 16:13:40 +0100

    Add GL constants for ETC2 and ASTC texture compression

--- libs/sdl/sdl/GL.hx
+++ libs/sdl/sdl/GL.hx
@@ -570,6 +570,48 @@ class GL {
         public static inline var COMPRESSED_RGBA_PVRTC_4BPPV1_IMG	= 0x8C02;
         public static inline var COMPRESSED_RGBA_PVRTC_2BPPV1_IMG	= 0x8C03;
 
+	/* ETC2 / EAC */
+	public static inline var COMPRESSED_RGB8_ETC2           		= 0x9274;
+	public static inline var COMPRESSED_SRGB8_ETC2          		= 0x9275;
+	public static inline var COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2 	= 0x9276;
+	public static inline var COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2 	= 0x9277;
+	public static inline var COMPRESSED_RGBA8_ETC2_EAC      		= 0x9278;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ETC2_EAC 		= 0x9279;
+	public static inline var COMPRESSED_R11_EAC             		= 0x9270;
+	public static inline var COMPRESSED_SIGNED_R11_EAC      		= 0x9271;
+	public static inline var COMPRESSED_RG11_EAC            		= 0x9272;
+	public static inline var COMPRESSED_SIGNED_RG11_EAC     		= 0x9273;
+
+	/* ASTC */
+	public static inline var COMPRESSED_RGBA_ASTC_4x4   		= 0x93B0;
+	public static inline var COMPRESSED_RGBA_ASTC_5x4   		= 0x93B1;
+	public static inline var COMPRESSED_RGBA_ASTC_5x5   		= 0x93B2;
+	public static inline var COMPRESSED_RGBA_ASTC_6x5 		= 0x93B3;
+	public static inline var COMPRESSED_RGBA_ASTC_6x6   		= 0x93B4;
+	public static inline var COMPRESSED_RGBA_ASTC_8x5   		= 0x93B5;
+	public static inline var COMPRESSED_RGBA_ASTC_8x6   		= 0x93B6;
+	public static inline var COMPRESSED_RGBA_ASTC_8x8   		= 0x93B7;
+	public static inline var COMPRESSED_RGBA_ASTC_10x5  		= 0x93B8;
+	public static inline var COMPRESSED_RGBA_ASTC_10x6  		= 0x93B9;
+	public static inline var COMPRESSED_RGBA_ASTC_10x8  		= 0x93BA;
+	public static inline var COMPRESSED_RGBA_ASTC_10x10 		= 0x93BB;
+	public static inline var COMPRESSED_RGBA_ASTC_12x10 		= 0x93BC;
+	public static inline var COMPRESSED_RGBA_ASTC_12x12 		= 0x93BD;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_4x4 	= 0x93D0;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_5x4 	= 0x93D1;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_5x5 	= 0x93D2;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_6x5 	= 0x93D3;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_6x6 	= 0x93D4;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_8x5 	= 0x93D5;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_8x6 	= 0x93D6;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_8x8 	= 0x93D7;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_10x5 	= 0x93D8;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_10x6 	= 0x93D9;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_10x8 	= 0x93DA;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_10x10 	= 0x93DB;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_12x10 	= 0x93DC;
+	public static inline var COMPRESSED_SRGB8_ALPHA8_ASTC_12x12 	= 0x93DD;
+
 	/* Shaders */
 	public static inline var FRAGMENT_SHADER                  = 0x8B30;
 	public static inline var VERTEX_SHADER                    = 0x8B31;
