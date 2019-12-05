5e55c318d3ca38a5abf2cc25eed3596a585f36f1
Iliyas Jorio <iliyas@evilempirestudio.com>
2019-12-05 15:13:06 +0100

    Install script: Don't copy x64 version

--- EE_CopyBuildToHaxeTools.bat
+++ EE_CopyBuildToHaxeTools.bat
@@ -3,7 +3,4 @@
 copy Release\hl.exe c:\haxetools\
 copy Release\*.dll c:\haxetools\
 copy Release\*.hdll c:\haxetools\
-copy x64\Release\hl.exe c:\haxetools\x64\
-copy x64\Release\*.dll c:\haxetools\x64\
-copy x64\Release\*.hdll c:\haxetools\x64\
 pause
